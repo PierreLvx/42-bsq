8bb408412e4f2b7ab25a873fa8f1500df5441aa3
Pierre LAVAUX <pierre.lavaux@student.42.fr>
2014-09-15 23:09:11 +0200

    buffer fix

--- srcs/libft/ft_read.c
+++ srcs/libft/ft_read.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_read.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/15 22:59:51 by plavaux           #+#    #+#             */
-/*   Updated: 2014/09/15 23:00:48 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/09/15 23:07:15 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int	ft_read(int fd)
 {
 	int		ret;
-	char	buff[BUFF_SIZE + 1];
+	char	buff[BUFFER_SIZE + 1];
 
-	while ((ret = read(fd, buff, BUFF_SIZE)))
+	while ((ret = read(fd, buff, BUFFER_SIZE)))
 	{
 		buff[ret] = '\0';
 		ft_putstr(buff);
