7e31101337306a45d7d0cb88a0789b21953fe6f1
Pierre LAVAUX <pierre.lavaux@student.42.fr>
2014-09-16 16:52:52 +0200

    cleanup and test main

--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,51 @@
 /*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/15 15:29:24 by plavaux           #+#    #+#             */
-/*   Updated: 2014/09/15 19:08:30 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/09/16 13:36:47 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libs.h"
+#include "includes/protos.h"
 
-int		main(int argc, char **argv)
+int	main(void)
 {
-	(void)argv;
-	if (argc == 1)
-		// READ FROM STDIN
+	int **array;
+	int i;
+	int j;
+	int *solution;
+
+	i = 0;
+	array = read_constraints("test.txt");
+	printf("0 - %d, 1 - %d, 2 - %d, 3 - %d", array[0][0], array[0][1], array[0][2], array[0][3]);
+	solution = get_square(array);
+	print_table(array, array[0][0], solution);
 	return (0);
 }
+
+/*
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	(void)argv;
+	if (argc == 1)
+		disp_stdin();
+	else
+	{
+		while (i < argc)
+		{
+			file = open(argv[i], O_RDONLY);
+			if (file >= 0)
+			{
+			while (read(file, &buffer, 1) != 0)
+				write(1, &buffer, 1);
+			}
+		}
+		close(file);
+		i++;
+	}
+	return (0);
+}
+*/
--- srcs/printtable.c
+++ srcs/printtable.c
@@ -3,54 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   printtable.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/16 06:20:12 by fschuber          #+#    #+#             */
-/*   Updated: 2014/09/16 07:01:03 by fschuber         ###   ########.fr       */
+/*   Updated: 2014/09/16 15:09:57 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "includes/libs.h"
+#include "includes/protos.h"
 
-void ft_putchar(char c)
+int		is_in(int **table, int x, int y)
 {
-	write(1, &c, 1);
-}
-
-int is_in(int **table, int x, int y)
-{
-	int i;
+	int	i;
 
 	i = 0;
-	while(table[y][i] != -1)
+	while (table[y][i] != -1)
 	{
 		if (table[y][i] == x)
-			return 1;
+			return (1);
 		i++;
 	}
-	return 0;
+	return (0);
 }
 
-void print_table(int **table, int size, int *solution)
+void	print_table(int **table, int size, int *solution)
 {
-	int i;
-	int j;
-	int k;
-	int type;
+	int	i;
+	int	j;
+	int	k;
+	int	type;
 
-	j = 0;
+	j = 1;
 	k = 0;
-	while (j < size)
+	while (j <= size)
 	{
 		i = 0;
 		while (i < size)
 		{
-			if (i >= solution[0] && i < solution[0] + solution[2] && j >= solution[1] && j < solution[1] + solution[2])
-				ft_putchar('x');
+			if (i >= solution[0] && i < solution[0] + solution[2]
+					&& j >= solution[1] && j < solution[1] + solution[2])
+				ft_putchar(table[0][3]);
 			else if (is_in(table, i, j))
-				ft_putchar ('o');
+				ft_putchar (table[0][2]);
 			else
-				ft_putchar ('.');
+				ft_putchar (table[0][1]);
 			i++;
 		}
 		ft_putchar('\n');
