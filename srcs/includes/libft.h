b15be3359f22a7895e53b20f1d6cab57e8b6809b
Pierre LAVAUX <pierre.lavaux@student.42.fr>
2014-09-17 16:10:11 +0200

    removing unused functions

--- srcs/includes/libft.h
+++ srcs/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/15 15:34:24 by plavaux           #+#    #+#             */
-/*   Updated: 2014/09/16 13:20:55 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/09/17 15:57:45 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ void	ft_putchar(char str);
 void	ft_putnbr(int s);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
-
-int		ft_close(int fd);
-int		ft_open(char *file);
-int		ft_read(int fd);
 
 void	change_number(int *num, int *factor, char *ptr);
 int		ft_atoi(char *str);
